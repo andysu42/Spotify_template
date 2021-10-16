59aea8663c303c186002bbe78f71d9da6a0f94be
andysu42 <nocab915@gmail.com>
2021-10-17 01:38:05 +0800

    add: button, menu components

---
+++ src/components/common/Buttons.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .sp__btn(:class="[bgColor, textColor, disabledColor]" :disabled="disableButton")
+    slot {{ text }}
+</template>
+<script>
+export default {
+  name: 'btn',
+  props: {
+    bgColor: {
+      type: String,
+      default: 'bg-default-green'
+    },
+    textColor: {
+      type: String,
+      default: 'text-black-100'
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      disableButton: this.disabled,
+      text: 'default text'
+    }
+  },
+  computed: {
+    disabledColor () {
+      return this.disabled ? 'opacity-50' : ''
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.sp__btn {
+  @apply text-center text-sm font-bold cursor-pointer overflow-hidden;
+}
+</style>
---
+++ src/components/common/Topmenu.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+div.py-2.relative(:class="bgColor")
+  .flex.flex-row.justify-between
+    div.pr-1
+      .flex.flex-row
+        .flex-1
+          .flex.items-center.justify-center.rounded-full.mx-1.py-2.h-8.w-8.bg-black.text-white.cursor-pointer ＜
+        .flex-1
+          .flex.items-center.justify-center.rounded-full.mx-1.py-2.h-8.w-8.bg-black.text-white.cursor-pointer ＞
+        .flex-1
+          slot(name="center")
+            div.flex.h-8 default
+    div.pr-1
+      .flex.h-8
+        div.flex.items-center
+          div(class="sm:hidden md:hidden lg:flex" style="width:150px").flex.items-center.justify-center.rounded-full.py-2.px-3.bg-black.text-white.text-center.text-sm.font-bold IMG 蘇楷倫 LOGO
+</template>
+<script>
+export default {
+  name: 'Topmenu',
+  props: {
+    bgColor: {
+      type: String,
+      default: 'bg-transparent'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped></style>
